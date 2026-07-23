d81deca2ea1fabd7676313deba52e178acbc5a7a
José Tomé <josetome.cpy@gmail.com>
2026-07-23 18:50:37 -0300

    feat: Hero de Home

---
+++ frontend/src/pages/Home/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import "./Hero.css"
+
+export function Hero(){
+
+    return(
+
+        <div className="hero">
+            <div className="hero__container">
+                <img src="/img/resources/hero.png"/> 
+
+                <p>Encontrá los mejores precios en</p>
+                <h1>Imponet</h1>
+                <p>Productos Seleccionados</p>
+            </div>
+
+            
+        </div>
+
+    )
+
+}
