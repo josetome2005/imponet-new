42cb48b510941f307de742c740abe2169a6ed3cf
José Tomé <josetome.cpy@gmail.com>
2026-08-06 21:37:40 -0300

    Pequeño edit de diseño a botones de tabla de categorías

--- frontend/src/pages/AdminCategorias/data/categorias.config.jsx
+++ frontend/src/pages/AdminCategorias/data/categorias.config.jsx
@@ -24,10 +24,10 @@ export const categorias_columns = (onEdit, onDelete) => [
         name: "",
         render: (p) => (
             <div className="flex--16 y-center actions__container">
-                <span className="material-symbols-outlined icon edit__icon" onClick={() => onEdit(p)}>
+                <span className="material-symbols-outlined icon edit__icon" onClick={() => onEdit(p)} title="Editar">
                     edit
                 </span>
-                <span className="material-symbols-outlined icon delete__icon" onClick={() => onDelete(p.id)}>
+                <span className="material-symbols-outlined icon delete__icon" onClick={() => onDelete(p.id)} title="Eliminar">
                     delete
                 </span>
             </div>
